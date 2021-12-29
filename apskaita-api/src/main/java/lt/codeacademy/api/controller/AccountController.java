a48220072511f9bb777492abbef242a634b3cc6e
stanislavasl <48596171+stanislavasl@users.noreply.github.com>
2021-12-29 16:37:59 +0200

    add application, controller

---
+++ apskaita-api/src/main/java/lt/codeacademy/api/controller/AccountController.java
@@ -0,0 +1,16 @@
+package lt.codeacademy.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class AccountController {
+
+    @GetMapping
+    public String sayHi() {
+        return "labas";
+    }
+
+}
